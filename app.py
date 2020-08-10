dc1381f5fcd27625a780adabec62c000120fd6e5
Filip Crnobrnja <fica.c@yandex.ru>
2020-08-10 11:22:16 +0300

    Update some structure

--- app.py
+++ app.py
@@ -1,33 +1,44 @@
-from telebot import types
-import telebot
-import logging
 
-logger = telebot.logger
-logger.setLevel(logging.DEBUG)
+import telebot
+import markups as m
+
+# import logging
+
+# logger = telebot.logger
+# logger.setLevel(logging.DEBUG)
 
 TOKEN = "1348328650:AAHW2tlpYyhl_8C-J9QxPSc3mTaowen7ps0"
 
-CUR_LANGUAGE="English"
-
-bot = telebot.TeleBot(TOKEN, parse_mode=None)
+bot = telebot.TeleBot(TOKEN)#, parse_mode=None)
+isRunning = False
+curLanguage = "English"
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-	markup.add('English')
-	msg = bot.send_message(message.chat.id, "Hello, my name is MeanIt! I can explain to you any word. Please select your preferred language:",\
-	reply_markup=markup)
-	bot.register_next_step_handler(msg, set_language)
+	global isRunning
+	if not isRunning:
+		msg = bot.send_message(message.chat.id, "Hello, my name is MeanIt! I can explain to you any word.")
+		isRunning = True
+		setLanguage(msg)
 
-def set_language(message):
+@bot.message_handler(commands=['setLanguage'])
+def setLanguage(message):
+	msg = bot.send_message(message.chat.id, "Please select your preferred language:", reply_markup=m.language_markup)
 	if (message.text.lower == 'english'):
-		CUR_LANGUAGE = "English"
-	msg = bot.send_message(message.chat.id, "Please enter an unknown word:")
-	bot.register_next_step_handler(msg, find_word)
+		curLanguage = "English"
+	bot.register_next_step_handler(msg, findWord)
+
+@bot.message_handler(commands=['findWord'])
+def findWord(message):
+	to_send = "Current language is " + curLanguage + ". Please enter an unknown word:"
+	msg = bot.send_message(message.chat.id, to_send)
+	bot.register_next_step_handler(msg, handle_word)
 
 @bot.message_handler(content_types=['text'])
-def find_word(message):
+def handle_word(message):
 	bot.send_message(message.chat.id,"Ok")
 
 if __name__ == '__main__':
-	bot.polling(none_stop=True, interval=0)
+	bot.polling(none_stop=True)#, interval=0)
+# handle /help
+# handle /settings
---
+++ markups.py
@@ -0,0 +1,4 @@
+from telebot import types
+
+language_markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+language_markup.add('English')
