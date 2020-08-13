49d8df4bd823f370e3365e19af12b076b2da3252
Adolfo Ashara <aashara-@il-a3.msk.21-school.ru>
2020-08-14 00:16:33 +0300

    Update handling messages
    * Add inline keyboard
    * Add flags to languages

--- app.py
+++ app.py
@@ -2,48 +2,56 @@
 import telebot
 import markups as m
 
-import logging
-
-logger = telebot.logger
-logger.setLevel(logging.DEBUG)
-
 TOKEN = "1348328650:AAHW2tlpYyhl_8C-J9QxPSc3mTaowen7ps0"
 
 bot = telebot.TeleBot(TOKEN)
 curLanguage = ""
+isStart = False
+OFFSET = 127462 - ord('A')
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start'], func = lambda m: isStart == False)
 def send_welcome(message):
+	global isStart
 	msg = bot.send_message(message.chat.id, "I can explain to you any word.")
 	msg = bot.send_message(message.chat.id, "Please select your preferred language:", reply_markup=m.language_markup)
+	isStart = True
 
-@bot.message_handler(commands=['English'])
+def flag(code):
+	code = code.upper()
+	return chr(ord(code[0]) + OFFSET) + chr(ord(code[1]) + OFFSET)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+	if call.data == "English":
+		setEnglishLanguage(call.message)
+	elif call.data == "French":
+		setFrenchLanguage(call.message)
+	elif call.data == "German":
+		setGermanLanguage(call.message)
+
+@bot.message_handler(commands=['English'], func = lambda m: curLanguage != "English")
 def setEnglishLanguage(message):
+	global curLanguage
 	if curLanguage != "English":
 		curLanguage = "English"
-		msg = bot.send_message(message.chat.id, "Now I know English.")
+		answer = "Now I know English" + flag('gb') + "."
+		msg = bot.send_message(message.chat.id, answer)
 
-@bot.message_handler(commands=['French'])
+@bot.message_handler(commands=['French'], func = lambda m: curLanguage != "French")
 def setFrenchLanguage(message):
+	global curLanguage
 	if curLanguage != "French":
 		curLanguage = "French"
-		msg = bot.send_message(message.chat.id, "Maintenant je connais le français.")
+		answer = "Maintenant je connais le français" + flag('fr') + "."
+		msg = bot.send_message(message.chat.id, answer)
 
-@bot.message_handler(commands=['German'])
+@bot.message_handler(commands=['German'], func = lambda m: curLanguage != "German")
 def setGermanLanguage(message):
+	global curLanguage
 	if curLanguage != "German":
 		curLanguage = "German"
-		msg = bot.send_message(message.chat.id, "Jetzt kenne ich die Deutsche Sprache.")
-
-@bot.message_handler(content_types=['text'])
-def handle_word(message):
-	if (message.text.lower == 'english'):
-		setEnglishLanguage(message)
-	elif (message.text.lower == 'french'):
-		setFrenchLanguage(message)
-	elif (message.text.lower == 'german'):
-		setGermanLanguage(message)
-	# bot.send_message(message.chat.id,"Ok")
+		answer = "Jetzt kenne ich die Deutsche Sprache" + flag('de') + "."
+		msg = bot.send_message(message.chat.id, answer)
 
 if __name__ == '__main__':
-	bot.polling(none_stop=True)#, interval=0)
+	bot.polling(none_stop=True, interval=0)
--- markups.py
+++ markups.py
@@ -1,6 +1,9 @@
 from telebot import types
 
-language_markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-language_markup.add('English')
-language_markup.add('French')
-language_markup.add('German')
+language_markup = types.InlineKeyboardMarkup()
+english_key = types.InlineKeyboardButton(text='English', callback_data='English')
+french_key = types.InlineKeyboardButton(text='French', callback_data='French')
+german_key = types.InlineKeyboardButton(text='German', callback_data='German')
+language_markup.add(english_key)
+language_markup.add(french_key)
+language_markup.add(german_key)
