43682ed501d2b89698aeb07cc0415d6d6cc23c5f
Filip Crnobrnja <fica.c@yandex.ru>
2020-08-13 12:51:08 +0300

    Start new logic

--- app.py
+++ app.py
@@ -2,43 +2,48 @@
 import telebot
 import markups as m
 
-# import logging
+import logging
 
-# logger = telebot.logger
-# logger.setLevel(logging.DEBUG)
+logger = telebot.logger
+logger.setLevel(logging.DEBUG)
 
 TOKEN = "1348328650:AAHW2tlpYyhl_8C-J9QxPSc3mTaowen7ps0"
 
-bot = telebot.TeleBot(TOKEN)#, parse_mode=None)
-isRunning = False
-curLanguage = "English"
+bot = telebot.TeleBot(TOKEN)
+curLanguage = ""
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	global isRunning
-	if not isRunning:
-		msg = bot.send_message(message.chat.id, "Hello, my name is MeanIt! I can explain to you any word.")
-		isRunning = True
-		setLanguage(msg)
-
-@bot.message_handler(commands=['setLanguage'])
-def setLanguage(message):
+	msg = bot.send_message(message.chat.id, "I can explain to you any word.")
 	msg = bot.send_message(message.chat.id, "Please select your preferred language:", reply_markup=m.language_markup)
-	if (message.text.lower == 'english'):
-		curLanguage = "English"
-	bot.register_next_step_handler(msg, findWord)
 
-@bot.message_handler(commands=['findWord'])
-def findWord(message):
-	to_send = "Current language is " + curLanguage + ". Please enter an unknown word:"
-	msg = bot.send_message(message.chat.id, to_send)
-	bot.register_next_step_handler(msg, handle_word)
+@bot.message_handler(commands=['English'])
+def setEnglishLanguage(message):
+	if curLanguage != "English":
+		curLanguage = "English"
+		msg = bot.send_message(message.chat.id, "Now I know English.")
+
+@bot.message_handler(commands=['French'])
+def setFrenchLanguage(message):
+	if curLanguage != "French":
+		curLanguage = "French"
+		msg = bot.send_message(message.chat.id, "Maintenant je connais le français.")
+
+@bot.message_handler(commands=['German'])
+def setGermanLanguage(message):
+	if curLanguage != "German":
+		curLanguage = "German"
+		msg = bot.send_message(message.chat.id, "Jetzt kenne ich die Deutsche Sprache.")
 
 @bot.message_handler(content_types=['text'])
 def handle_word(message):
-	bot.send_message(message.chat.id,"Ok")
+	if (message.text.lower == 'english'):
+		setEnglishLanguage(message)
+	elif (message.text.lower == 'french'):
+		setFrenchLanguage(message)
+	elif (message.text.lower == 'german'):
+		setGermanLanguage(message)
+	# bot.send_message(message.chat.id,"Ok")
 
 if __name__ == '__main__':
 	bot.polling(none_stop=True)#, interval=0)
-# handle /help
-# handle /settings
--- markups.py
+++ markups.py
@@ -2,3 +2,5 @@ from telebot import types
 
 language_markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
 language_markup.add('English')
+language_markup.add('French')
+language_markup.add('German')
